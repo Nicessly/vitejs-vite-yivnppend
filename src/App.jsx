feb9275a2157f9269becd6209b8253972e8b39e8
Nicessly <nicessly3@gmail.com>
2024-04-27 18:40:03 -0300

    hecho el nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Nav from './nav/nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-        <h1>Vite + React</h1>
-      </div>
+      <Nav/>
   )
 }
 
---
+++ src/nav/nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './nav.css'; 
+import logo from './logoz.png'
+import perfil from './perfil.png'
+import logo2 from './logoy.png'
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+        <span className="logo-text">ZYPHY</span>
+      </div>
+      <div className="right-section">
+        <img src={perfil} alt="Foto de Perfil" className="profile-picture" />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
