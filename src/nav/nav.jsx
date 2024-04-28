8dacce35cbf7099a360d561f3df07f3ee43d9196
Nicessly <147267903+Nicessly@users.noreply.github.com>
2024-04-28 19:01:32 -0300

    Update nav.jsx que cmabia de tema

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -1,15 +1,18 @@
-
 import React, { useState } from 'react';
 import './nav.css'; 
 import logo from './logoz.png';
 import perfil from './perfil.png';
+import logo2 from './logoy.png';
 import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import Tooltip from '@mui/material/Tooltip';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import { Dialog, DialogContent, DialogActions, Button } from '@mui/material';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import NightsStayIcon from '@mui/icons-material/NightsStay';
 
 function Nav() {
   const [openLogoutDialog, setOpenLogoutDialog] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   const handleOpenLogoutDialog = () => {
     setOpenLogoutDialog(true);
@@ -24,21 +27,25 @@ function Nav() {
     setOpenLogoutDialog(false);
   };
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${darkMode ? 'dark-theme' : 'light-theme'}`}>
       <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
         <span className="logo-text">ZYPHY</span>
       </div>
       <div className="right-section">
-        <Tooltip title="Settings" placement="bottom">
-          <button className='i'>
-            <SettingsIcon />
+        <Tooltip title="Theme" placement="bottom">
+          <button className='i' onClick={toggleDarkMode}>
+            {darkMode ? <Brightness7Icon style={{ color: '#fff' }} /> : <NightsStayIcon style={{ color: '#000' }} />}
           </button>
         </Tooltip>
         <Tooltip title="Logout" placement="bottom">
           <button className='i' onClick={handleOpenLogoutDialog}>
-            <LogoutIcon />
+            <LogoutOutlinedIcon style={{ color: darkMode ? '#fff' : '#000' }} />
           </button>
         </Tooltip>
       </div>
@@ -47,16 +54,16 @@ function Nav() {
       <Dialog open={openLogoutDialog} onClose={handleCloseLogoutDialog} classes={{ paper: 'custom-dialog-paper' }}>
         <DialogContent className="custom-dialog-title">Log Out?</DialogContent>
         <DialogContent className="custom-dialog-content">
-        Are you sure you want to log out?
+          Are you sure you want to log out?
         </DialogContent>
         <DialogActions className="custom-dialog-actions">
-  <Button onClick={handleCloseLogoutDialog} sx={{ backgroundColor: "#ABADAB"}}>
-    Cancel
-  </Button>
-  <Button onClick={handleLogout} sx={{ backgroundColor: "#5EFF33" }}>
-    Log Out
-  </Button>
-</DialogActions>
+          <Button onClick={handleCloseLogoutDialog} sx={{ backgroundColor: "#939393"}}>
+            Cancel
+          </Button>
+          <Button onClick={handleLogout} sx={{ backgroundColor: "#fca94b" }}>
+            Log Out
+          </Button>
+        </DialogActions>
       </Dialog>
     </nav>
   );
